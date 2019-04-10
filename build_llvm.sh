a898e1bc2459f62c1312068254e58c58b9217339
Henning Perl <perl@code-intelligence.de>
2019-04-10 18:00:04 +0200

    Merge branch 'master' into 564-create-command-to-generate-base-structure2
    # Conflicts:
    #	ui/vscode/package.json
    
    GitOrigin-RevId: 1ac9657ef938257944df9a45c2902d55d5ab1ccc

--- build_llvm.sh
+++ build_llvm.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+export CC=clang
+export CXX=clang++
+
 set -eo pipefail
 
 LLVM_RELEASE=${LLVM_RELEASE:-}
@@ -10,49 +13,39 @@ then
   exit -1
 fi
 
+SRC_DIR=llvm-project-llvmorg-${LLVM_RELEASE}
+if [[ ! -e ${SRC_DIR} ]]
+then
+    wget https://github.com/llvm/llvm-project/archive/llvmorg-${LLVM_RELEASE}.tar.gz
+    tar xf llvmorg-${LLVM_RELEASE}.tar.gz
+fi
+
 OUT_DIR=llvm-${LLVM_RELEASE}
 
-gpg --import ../../gpg_keys/*.gpg
-
-fetch_component () {
-  COMPONENT=$1
-  BASE_URL=http://releases.llvm.org/${LLVM_RELEASE}
-  PKG=${COMPONENT}-${LLVM_RELEASE}.src.tar.xz
-  SIG=${COMPONENT}-${LLVM_RELEASE}.src.tar.xz.sig
-
-  if [ ! -e src/${COMPONENT} ]
-  then
-    wget ${BASE_URL}/${PKG}
-    wget ${BASE_URL}/${SIG}
-    gpg --verify ${SIG} ${PKG}
-    tar xf ${PKG}
-    mv ${COMPONENT}-${LLVM_RELEASE}.src src/${COMPONENT}
-    rm -f ${PKG} ${SIG}
-  fi
-}
-
-mkdir -p src
 mkdir -p build
 mkdir -p ${OUT_DIR}
 
-fetch_component llvm
-fetch_component compiler-rt
-fetch_component cfe
-
 pushd build
 
-cmake ../src/llvm \
+cmake ../${SRC_DIR}/llvm \
       -DCMAKE_BUILD_TYPE="Release" \
       -DLLVM_TARGETS_TO_BUILD="X86" \
+      -DLLVM_ENABLE_PROJECTS="clang;clang-tools-extra;libcxx;libcxxabi;compiler-rt;libunwind;lld" \
       -DLLVM_BUILD_TOOLS=ON \
       -DBUILD_SHARED_LIBS=ON \
       -DLLVM_INCLUDE_EXAMPLES=OFF \
       -DLLVM_INCLUDE_TESTS=ON \
-      -DCMAKE_INSTALL_PREFIX=../${OUT_DIR} \
-      -DLLVM_EXTERNAL_CLANG_SOURCE_DIR=../src/cfe \
-      -DLLVM_EXTERNAL_COMPILER_RT_SOURCE_DIR=../src/compiler-rt
+      -DLLVM_BUILD_TESTS=ON \
+      -DLLVM_ENABLE_BINDINGS=OFF \
+      -DLLVM_ENABLE_OCAMLDOC=OFF \
+      -DLLVM_TEMPORARILY_ALLOW_OLD_TOOLCHAIN=ON \
+      -DLIBCXXABI_USE_LLVM_UNWINDER=YES \
+      -DLIBCXXABI_USE_COMPILER_RT=YES \
+      -DLIBCXX_USE_COMPILER_RT=YES \
+      -DCMAKE_INSTALL_PREFIX=../${OUT_DIR}
 
-make -j3
+
+make -j8
 cmake --build . --target install
 
 popd
